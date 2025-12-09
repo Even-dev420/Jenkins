221e1f5d8de9df3b647c2e68ced06fb059bc994f
Even <igorshashkov2005@yandex.ru>
2025-12-09 21:16:29 +0300

    tests

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,31 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+
+    @Test
+    public void testResetAfterAddition() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(10.5);
+        agr.addValue(20.3);
+        assertThat(agr.getSum()).isEqualTo(30.8);
+        
+        agr.reset();
+        assertThat(agr.getSum()).isZero();
+    }
+    
+    @Test
+    public void testResetAndAddAgain() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(100);
+        agr.reset();
+        agr.addValue(50);
+        assertThat(agr.getSum()).isEqualTo(50);
+    }
+    
+    @Test
+    public void testResetWhenAlreadyZero() {
+        Aggregator agr = new Aggregator();
+        agr.reset(); // reset когда сумма уже 0
+        assertThat(agr.getSum()).isZero();
+    }
 }
